5104aeef4ce045d2ba44a7f0eb3b689caecfcf78
123456375 <78317775+123456375@users.noreply.github.com>
2021-02-01 23:22:39 -0800

    Add files via upload

---
+++ BreacknContinue.java
@@ -0,0 +1,10 @@
+package dae2;
+
+public class BreacknContinue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ UserInputDemo.java
@@ -0,0 +1,16 @@
+package dae2;
+
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	private Object system;
+
+	public static void main(String[]args) {
+    Scanner scan=new Scanner(system.in);
+    system.out.println("enter your id");
+    int id=scan.nextInt();
+    system.out.println("id is"+id);
+	}
+}
+
